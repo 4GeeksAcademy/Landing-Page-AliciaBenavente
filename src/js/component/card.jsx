ab3019a688c80e4cce1f808ce1e8c874d598e914
Alicia Benavente <aliciabenaventegarcia@gmail.com>
2024-08-14 11:38:00 +0000

    responsive style

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -4,37 +4,37 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 const Card = () => {
     return(
-        <div className="mb-3 row container-fluid justify-content-center" style={{width: "70%", marginLeft:"15%"}}>
-            <div className="card me-2" style={{width: "24%"}}>
+        <div className="mb-3 row container-fluid justify-content-center d-flex gap-1" style={{width: "70%", marginLeft:"15%"}}>
+             <div className="card col-xxl-2 col-xl-3 col-lg-4 col-md-5 col-sm-6">
                 <img src={rigoImage} className="card-img-top" alt="Rigo Baby Image" />
                 <div className="card-body">
                     <h5 className="card-title">Card title</h5>
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                    <button className="btn btn-primary btn-sm col-12 mx-auto">Go somewhere</button>
                 </div>
             </div>
-            <div className="card me-2" style={{width: "24%"}}>
+            <div className="card col-xxl-2 col-xl-3 col-lg-4 col-md-5 col-sm-6">
                 <img src={rigoImage} className="card-img-top" alt="Rigo Baby Image" />
                 <div className="card-body">
                     <h5 className="card-title">Card title</h5>
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                    <button className="btn btn-primary btn-sm col-12 mx-auto">Go somewhere</button>
                 </div>
             </div>
-            <div className="card me-2" style={{width: "24%"}}>
+            <div className="card col-xxl-2 col-xl-3 col-lg-4 col-md-5 col-sm-6">
                 <img src={rigoImage} className="card-img-top" alt="Rigo Baby Image" />
                 <div className="card-body">
                     <h5 className="card-title">Card title</h5>
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                    <button className="btn btn-primary btn-sm col-12 mx-auto">Go somewhere</button>
                 </div>
             </div>
-            <div className="card" style={{width: "24%"}}>
+            <div className="card col-xxl-2 col-xl-3 col-lg-4 col-md-5 col-sm-6 mb-2">
                 <img src={rigoImage} className="card-img-top" alt="Rigo Baby Image" />
                 <div className="card-body">
                     <h5 className="card-title">Card title</h5>
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                    <button className="btn btn-primary btn-sm col-12 mx-auto">Go somewhere</button>
                 </div>
             </div>
         </div>
